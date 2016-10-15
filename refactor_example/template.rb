0a64e91b69506e7040fcd63d46cbab0184c4d32b
Thomas Claiborne <thomas.claiborne3@gmail.com>
2016-10-14 18:30:54 -0700

    Refactored and initial folder setup for rss-reader

--- refactor_example/template.rb
+++ refactor_example/template.rb
@@ -1,26 +1,40 @@
 module Template
-  def template(source_template, req_id)
-    template = String.new(source_template)
+  def template(source_template, req_id, alt_options)
 
-    # Substitute for %CODE%
-    template_split_begin = template.index("%CODE%")
-    template_split_end = template_split_begin + 6
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    code = String.new(req_id)
-    template =
-      String.new(template_part_one + code + template_part_two)
+    replace_alt_code = format_altcode(req_id, alt_options)
 
-    # Substitute for %ALTCODE%
-    template_split_begin = template.index("%ALTCODE%")
-    template_split_end = template_split_begin + 9
-    template_part_one =
-      String.new(template[0..(template_split_begin-1)])
-    template_part_two =
-      String.new(template[template_split_end..template.length])
-    altcode = code[0..4] + "-" + code[5..7]
-    template_part_one + altcode + template_part_two
+    new_template = replace_word(source_template, '%CODE%', req_id)
+    replace_word(new_template, '%ALTCODE%', replace_alt_code)
+  end
+
+  private
+  # no need to find the index by hand and split the characters
+  # accordingly when we can do a simple pattern match with regular exp
+
+  # gsub does all the previous work under the hood in it's implementation
+
+  # this makes our code more reusable and we can alter our matching
+  def replace_word(template, replaced_word, new_word)
+    template.gsub(/#{replaced_word}/, new_word)
+  end
+  # we define a private method format altcode if we want to later
+  # change our default options for how our altcode is modified
+
+  # this should not be a public interface but an implementation detail
+  # we define default options that can quickly be changed and the ability
+  # to pass options as we see fit using a hash
+  def format_altcode(req_id, options = {})
+    dash_entry      = options[:dash_entry]      || default_dash_entry
+    alt_code_length = options[:alt_code_length] || default_alt_code_length
+
+    replace_alt_code = req_id[0...dash_entry] + "-" + req_id[dash_entry...alt_code_length]
+  end
+
+  def default_dash_entry
+    5
+  end
+
+  def default_alt_code_length
+    8
   end
 end
